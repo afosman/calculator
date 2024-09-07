16854b36d43d539ce028aae106259263db695c33
afosman <afosman2016@gmail.com>
2024-09-07 12:46:15 +0000

    Add Spring Boot skeleton

---
+++ src/test/java/com/afosman/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afosman.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
