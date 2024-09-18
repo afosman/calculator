c733ab07fbfe99f1b3382f3a867878ebac6f5135
afosman <afosman2016@gmail.com>
2024-09-18 15:23:25 +0000

    changed the test to get the IP of the container dynamically due to my local setup

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+# test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+
+test $(curl $(docker inspect -f '{{ .NetworkSettings.IPAddress }}' calculator ):8081/sum?a=1\&b=2) -eq 3
